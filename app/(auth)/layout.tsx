9fbb22e0e0e261ad6894cb9baab2bec5f3d60ef2
Israel Rotimi <izzyrotimi@gmail.com>
2025-04-18 17:05:47 +0100

    fixed redirect bug on sign-in page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from "react";
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
 
-  if (isUserAuthenticated) redirect("/sign-in");
+  if (isUserAuthenticated) redirect("/");
   return <div className="auth-layout">{children}</div>;
 };
 
