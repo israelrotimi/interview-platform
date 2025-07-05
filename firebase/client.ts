c30a616f0bd21642fcce4692c2f82c8845e2b3d7
Israel Rotimi <izzyrotimi@gmail.com>
2025-07-05 02:47:07 +0100

    fix invalid api key bug

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
+  apiKey: "AIzaSyDG8wCzOeDXIEDUGj20rUtluT_r5TGLI5Q", // Only works if it's placed here directly not in .env
   authDomain: "interviewprep-2c518.firebaseapp.com",
   projectId: "interviewprep-2c518",
   storageBucket: "interviewprep-2c518.firebasestorage.app",
   messagingSenderId: "139775316497",
   appId: "1:139775316497:web:9045985ef3ebc7a68057b0",
-  measurementId: "G-82GZWB084F",
+  measurementId: "G-82GZWB084F"
 };
 
 // Initialize Firebase
